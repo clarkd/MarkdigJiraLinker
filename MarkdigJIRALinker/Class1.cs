c5eb6f1152ccb23604e9839d5928e1f0be6a81b5
Dave Clarke <dave.clarke@squaredup.com>
2017-05-12 10:42:20 +0100

    Initial version - tested and working with Markdig 0.11.0

--- MarkdigJIRALinker/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MarkdigJIRALinker
-{
-    public class Class1
-    {
-    }
-}
---
+++ MarkdigJIRALinker/JiraLinkerExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Markdig;
+using Markdig.Parsers.Inlines;
+using Markdig.Renderers;
+
+namespace MarkdigJiraLinker
+{
+    /// <summary>
+    /// Simple inline parser extension for Markdig to find, and 
+    /// automatically add links to JIRA issue numbers.
+    /// </summary>
+    public class JiraLinkerExtension : IMarkdownExtension
+    {
+        private readonly JiraLinkOptions _options;
+
+        public JiraLinkerExtension(JiraLinkOptions options)
+        {
+            _options = options;
+        }
+
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            if (!pipeline.InlineParsers.Contains<JiraLinkInlineParser>())
+            {
+                // Insert the parser before the link inline parser
+                pipeline.InlineParsers.InsertBefore<LinkInlineParser>(new JiraLinkInlineParser());
+            }
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+            var htmlRenderer = renderer as HtmlRenderer;
+            if (htmlRenderer != null && !htmlRenderer.ObjectRenderers.Contains<JiraLinkRenderer>())
+            {
+                htmlRenderer.ObjectRenderers.Add(new JiraLinkRenderer(_options));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Add a extension method to add the extension to the pipeline
+    /// </summary>
+    public static class JiraLinkExtensionFunctions
+    {
+        public static MarkdownPipelineBuilder UseJiraLinks(this MarkdownPipelineBuilder pipeline, JiraLinkOptions options)
+        {
+            if (!pipeline.Extensions.Contains<JiraLinkerExtension>())
+            {
+                pipeline.Extensions.Add(new JiraLinkerExtension(options));
+            }
+            return pipeline;
+        }
+
+    }
+    
+}
